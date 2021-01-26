93e816554856364cfd64fe4d5847143e65f8994f
bhashinee <bhashineen@gmail.com>
2021-01-26 13:27:47 +0530

    Add module utils file and remove module version constant

---
+++ websocket-native/src/main/java/org/ballerinalang/net/websocket/ModuleUtils.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ballerinalang.net.websocket;
+
+import io.ballerina.runtime.api.Environment;
+import io.ballerina.runtime.api.Module;
+
+import static io.ballerina.runtime.api.constants.RuntimeConstants.ORG_NAME_SEPARATOR;
+
+/**
+ * This class will hold module related utility functions.
+ */
+public class ModuleUtils {
+
+    private static Module websocketModule;
+    private static String packageIdentifier;
+
+    private ModuleUtils() {}
+
+    public static void setModule(Environment env) {
+        websocketModule = env.getCurrentModule();
+        packageIdentifier = WebSocketConstants.PACKAGE + ORG_NAME_SEPARATOR + WebSocketConstants.PROTOCOL_WEBSOCKET +
+               WebSocketConstants.SEPARATOR + websocketModule.getVersion();
+    }
+
+    public static Module getWebsocketModule() {
+        return websocketModule;
+    }
+
+    public static String getPackageIdentifier() {
+        return packageIdentifier;
+    }
+}
